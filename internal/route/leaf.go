537299cc014dc4b6e8e5ca79c2f302d7f2b14d5b
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2021-05-03 02:26:59 +0800

    Finish tree construction methods

--- internal/route/leaf.go
+++ internal/route/leaf.go
@@ -17,18 +17,19 @@ type MatchStyle int8
 
 // NOTE: The order of types matters, which determines the matching priority.
 const (
-	matchStyleStatic      MatchStyle = iota // e.g. /webapi
-	matchStyleRegex                         // e.g. /webapi/{name: /[0-9]+/}
-	matchStylePlaceholder                   // e.g. /webapi/{name}
-	matchStyleAll                           // e.g. /webapi/{name: **}
+	matchStyleNone        MatchStyle = iota
+	matchStyleStatic                 // e.g. /webapi
+	matchStyleRegex                  // e.g. /webapi/{name: /[0-9]+/}
+	matchStylePlaceholder            // e.g. /webapi/{name}
+	matchStyleAll                    // e.g. /webapi/{name: **}
 )
 
 // Leaf is a leaf derived from a segment.
 type Leaf interface {
 	// Parent returns the parent tree the leaf belongs to.
-	Parent() *Tree
-	// Optional returns true if the leaf is optional.
-	Optional() bool
+	Parent() Tree
+	// Segment returns the segment that the leaf is derived from.
+	Segment() *Segment
 	// MatchStyle returns the match style of the leaf.
 	MatchStyle() MatchStyle
 	// URLPath fills in bind parameters with given values to build the "path"
@@ -39,18 +40,18 @@ type Leaf interface {
 
 // baseLeaf contains common fields for any leaf.
 type baseLeaf struct {
-	parent  *Tree    // The parent tree this leaf belongs to.
+	parent  Tree     // The parent tree this leaf belongs to.
 	route   *Route   // The route that the segment belongs to.
 	segment *Segment // The segment that the leaf is derived from.
 	handler Handler  // The handler bound to the leaf.
 }
 
-func (l *baseLeaf) Parent() *Tree {
+func (l *baseLeaf) Parent() Tree {
 	return l.parent
 }
 
-func (l *baseLeaf) Optional() bool {
-	return l.segment.Optional
+func (l *baseLeaf) Segment() *Segment {
+	return l.segment
 }
 
 func (l *baseLeaf) URLPath(vals map[string]string, withOptional bool) string {
@@ -129,54 +130,26 @@ func (l *matchAllLeaf) MatchStyle() MatchStyle {
 	return matchStyleAll
 }
 
-// newLeaf creates and returns a new Leaf derived from the given segment.
-func newLeaf(t *Tree, r *Route, s *Segment, h Handler) (Leaf, error) {
-	if len(s.Elements) == 0 {
-		return nil, errors.Errorf("empty segment in position %d", s.Pos.Offset)
-	}
+func isMatchStyleStatic(s *Segment) bool {
+	return len(s.Elements) == 1 && s.Elements[0].Ident != nil
+}
 
-	// Fast path for static route
-	if len(s.Elements) == 1 && s.Elements[0].Ident != nil {
-		return &staticLeaf{
-			baseLeaf: baseLeaf{
-				parent:  t,
-				route:   r,
-				segment: s,
-				handler: h,
-			},
-		}, nil
-	}
+func isMatchStylePlaceholder(s *Segment) bool {
+	return len(s.Elements) == 1 && s.Elements[0].BindIdent != nil
+}
 
-	// Fast path for placeholder
-	if len(s.Elements) == 1 && s.Elements[0].BindIdent != nil {
-		return &placeholderLeaf{
-			baseLeaf: baseLeaf{
-				parent:  t,
-				route:   r,
-				segment: s,
-				handler: h,
-			},
-		}, nil
-	}
-
-	// Fast path for match all
+func checkMatchStyleAll(s *Segment) (string, bool) {
 	if len(s.Elements) == 1 &&
 		s.Elements[0].BindParameters != nil &&
 		len(s.Elements[0].BindParameters.Parameters) == 1 &&
 		s.Elements[0].BindParameters.Parameters[0].Value.Literal != nil &&
 		*s.Elements[0].BindParameters.Parameters[0].Value.Literal == "**" {
-		return &matchAllLeaf{
-			baseLeaf: baseLeaf{
-				parent:  t,
-				route:   r,
-				segment: s,
-				handler: h,
-			},
-			bind: s.Elements[0].BindParameters.Parameters[0].Ident,
-		}, nil
+		return s.Elements[0].BindParameters.Parameters[0].Ident, true
 	}
+	return "", false
+}
 
-	// The only remaining style is regex
+func constructMatchStyleRegex(s *Segment) (*regexp.Regexp, []string, error) {
 	binds := make([]string, 0, len(s.Elements))
 	buf := bytes.NewBufferString("^")
 	for _, e := range s.Elements {
@@ -189,12 +162,12 @@ func newLeaf(t *Tree, r *Route, s *Segment, h Handler) (Leaf, error) {
 			buf.WriteString("(.+)")
 			continue
 		} else if e.BindParameters == nil || len(e.BindParameters.Parameters) == 0 {
-			return nil, errors.Errorf("empty segment element in position %d", e.Pos.Offset)
+			return nil, nil, errors.Errorf("empty segment element in position %d", e.Pos.Offset)
 		}
 
 		for _, p := range e.BindParameters.Parameters {
 			if p.Value.Regex == nil {
-				return nil, errors.Errorf("segment has non-regex literal in position %d", e.Pos.Offset)
+				return nil, nil, errors.Errorf("segment has non-regex literal in position %d", e.Pos.Offset)
 			}
 
 			binds = append(binds, p.Ident)
@@ -207,11 +180,59 @@ func newLeaf(t *Tree, r *Route, s *Segment, h Handler) (Leaf, error) {
 
 	re, err := regexp.Compile(buf.String())
 	if err != nil {
-		return nil, errors.Wrapf(err, "compile regexp near position %d", s.Pos.Offset)
+		return nil, nil, errors.Wrapf(err, "compile regexp near position %d", s.Pos.Offset)
+	}
+	return re, binds, nil
+}
+
+// newLeaf creates and returns a new Leaf derived from the given segment.
+func newLeaf(parent Tree, r *Route, s *Segment, h Handler) (Leaf, error) {
+	if len(s.Elements) == 0 {
+		return nil, errors.Errorf("empty segment in position %d", s.Pos.Offset)
+	}
+
+	if isMatchStyleStatic(s) {
+		return &staticLeaf{
+			baseLeaf: baseLeaf{
+				parent:  parent,
+				route:   r,
+				segment: s,
+				handler: h,
+			},
+		}, nil
+	}
+
+	if isMatchStylePlaceholder(s) {
+		return &placeholderLeaf{
+			baseLeaf: baseLeaf{
+				parent:  parent,
+				route:   r,
+				segment: s,
+				handler: h,
+			},
+		}, nil
+	}
+
+	if bind, ok := checkMatchStyleAll(s); ok {
+		return &matchAllLeaf{
+			baseLeaf: baseLeaf{
+				parent:  parent,
+				route:   r,
+				segment: s,
+				handler: h,
+			},
+			bind: bind,
+		}, nil
+	}
+
+	// The only remaining style is regex
+	re, binds, err := constructMatchStyleRegex(s)
+	if err != nil {
+		return nil, err
 	}
 	return &regexLeaf{
 		baseLeaf: baseLeaf{
-			parent:  t,
+			parent:  parent,
 			route:   r,
 			segment: s,
 			handler: h,
--- internal/route/tree.go
+++ internal/route/tree.go
@@ -6,20 +6,43 @@ package route
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/pkg/errors"
 )
 
 // todo
-type Tree struct {
-	parent  *Tree
-	segment *Segment
-	leaves  []Leaf
+type Tree interface {
+	// Parent returns the parent tree. The root tree does not have parent.
+	Parent() Tree
+	// Segment returns the segment that the tree is derived from.
+	Segment() *Segment
+	// MatchStyle returns the match style of the tree.
+	MatchStyle() MatchStyle
+	// todo
+	addNextSegment(r *Route, next int, h Handler) (Leaf, error)
+	// todo
+	addLeaf(r *Route, s *Segment, h Handler) (Leaf, error)
 }
 
 // todo
-func NewTree() *Tree {
-	return &Tree{}
+type baseTree struct {
+	parent   Tree     // The parent tree.
+	segment  *Segment // The segment that the tree is derived from.
+	subtrees []Tree   // The list of direct subtrees.
+	leaves   []Leaf   // The list of direct leaves.
+}
+
+func (t *baseTree) Parent() Tree {
+	return t.parent
+}
+
+func (t *baseTree) Segment() *Segment {
+	return t.segment
+}
+
+func (t *baseTree) MatchStyle() MatchStyle {
+	return matchStyleNone
 }
 
 // todo
@@ -28,24 +51,27 @@ type Params map[string]string
 // todo
 type Handler func(http.ResponseWriter, *http.Request, Params)
 
-// todo
-func (t *Tree) addLeaf(r *Route, s *Segment, h Handler) (Leaf, error) {
-	// todo: check if the same segment has been added already
+func (t *baseTree) addLeaf(r *Route, s *Segment, h Handler) (Leaf, error) {
+	for _, l := range t.leaves {
+		if l.Segment().String() == s.String() {
+			return l, nil
+		}
+	}
 
 	leaf, err := newLeaf(t, r, s, h)
 	if err != nil {
 		return nil, errors.Wrap(err, "new leaf")
 	}
 
-	if leaf.Optional() {
+	if leaf.Segment().Optional {
 		parent := leaf.Parent()
-		if parent.parent != nil {
-			_, err = parent.parent.addLeaf(r, parent.segment, h)
+		if parent.Parent() != nil {
+			_, err = parent.Parent().addLeaf(r, parent.Segment(), h)
 			if err != nil {
 				return nil, errors.Wrap(err, "add optional leaf to grandparent")
 			}
 		} else {
-			_, err = parent.addLeaf(r, parent.segment, h)
+			_, err = parent.addLeaf(r, parent.Segment(), h)
 			if err != nil {
 				return nil, errors.Wrap(err, "add optional leaf to parent")
 			}
@@ -69,12 +95,35 @@ func (t *Tree) addLeaf(r *Route, s *Segment, h Handler) (Leaf, error) {
 }
 
 // todo
-func (t *Tree) addSubtree(r *Route, next int, h Handler) (Leaf, error) {
-	return nil, nil // todo
+func (t *baseTree) addSubtree(r *Route, next int, h Handler) (Leaf, error) {
+	for _, st := range t.subtrees {
+		if st.Segment().String() == r.Segments[next].String() {
+			return st.addNextSegment(r, next+1, h)
+		}
+	}
+
+	subtree, err := newTree(t, r.Segments[next])
+	if err != nil {
+		return nil, errors.Wrap(err, "new tree")
+	}
+
+	// Determine subtree position by the priority of match styles.
+	i := 0
+	for ; i < len(t.subtrees); i++ {
+		if subtree.MatchStyle() < t.subtrees[i].MatchStyle() {
+			break
+		}
+	}
+
+	if i == len(t.subtrees) {
+		t.subtrees = append(t.subtrees, subtree)
+	} else {
+		t.subtrees = append(t.subtrees[:i], append([]Tree{subtree}, t.subtrees[i:]...)...)
+	}
+	return subtree.addNextSegment(r, next+1, h)
 }
 
-// todo
-func (t *Tree) addNextSegment(r *Route, next int, h Handler) (Leaf, error) {
+func (t *baseTree) addNextSegment(r *Route, next int, h Handler) (Leaf, error) {
 	if len(r.Segments) >= next+1 {
 		return t.addLeaf(r, r.Segments[next], h)
 	}
@@ -86,9 +135,105 @@ func (t *Tree) addNextSegment(r *Route, next int, h Handler) (Leaf, error) {
 }
 
 // todo
-func (t *Tree) AddRoute(r *Route, h Handler) (Leaf, error) {
+func (t *baseTree) AddRoute(r *Route, h Handler) (Leaf, error) {
 	if r == nil || len(r.Segments) == 0 {
 		return nil, errors.New("cannot add empty route")
 	}
 	return t.addNextSegment(r, 0, h)
+}
+
+// staticTree is a tree with a static match style.
+type staticTree struct {
+	baseTree
+}
+
+func (t *staticTree) MatchStyle() MatchStyle {
+	return matchStyleStatic
+}
+
+// regexTree is a tree with a regex match style.
+type regexTree struct {
+	baseTree
+	regexp *regexp.Regexp // The regexp for the tree.
+	binds  []string       // The list of bind parameters.
+}
+
+func (t *regexTree) MatchStyle() MatchStyle {
+	return matchStyleRegex
+}
+
+// placeholderTree is a tree with a placeholder match style.
+type placeholderTree struct {
+	baseTree
+}
+
+func (l *placeholderTree) MatchStyle() MatchStyle {
+	return matchStylePlaceholder
+}
+
+// placeholderTree is a tree with a match all style.
+type matchAllTree struct {
+	baseTree
+	bind string // The name of the bind parameter.
+}
+
+func (l *matchAllTree) MatchStyle() MatchStyle {
+	return matchStyleAll
+}
+
+// newTree creates and returns a new Tree derived from the given segment.
+func newTree(parent Tree, s *Segment) (Tree, error) {
+	if len(s.Elements) == 0 {
+		return nil, errors.Errorf("empty segment in position %d", s.Pos.Offset)
+	}
+
+	if isMatchStyleStatic(s) {
+		return &staticTree{
+			baseTree: baseTree{
+				parent:  parent,
+				segment: s,
+			},
+		}, nil
+	}
+
+	if isMatchStylePlaceholder(s) {
+		return &placeholderTree{
+			baseTree: baseTree{
+				parent:  parent,
+				segment: s,
+			},
+		}, nil
+	}
+
+	if bind, ok := checkMatchStyleAll(s); ok {
+		// One route can only have at most one match all style for subtree.
+		ancestor := parent
+		for ancestor != nil {
+			if ancestor.MatchStyle() == matchStyleAll {
+				return nil, errors.Errorf("duplicated match all style in position %d", s.Pos.Offset)
+			}
+		}
+
+		return &matchAllTree{
+			baseTree: baseTree{
+				parent:  parent,
+				segment: s,
+			},
+			bind: bind,
+		}, nil
+	}
+
+	// The only remaining style is regex
+	re, binds, err := constructMatchStyleRegex(s)
+	if err != nil {
+		return nil, err
+	}
+	return &regexTree{
+		baseTree: baseTree{
+			parent:  parent,
+			segment: s,
+		},
+		regexp: re,
+		binds:  binds,
+	}, nil
 }
