7c4a1994027634010df8c54b0edc6689c2c621c8
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2021-05-04 17:03:31 +0800

    Add Context

--- context.go
+++ context.go
@@ -4,8 +4,110 @@
 
 package flamego
 
-// todo
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/flamego/flamego/internal/inject"
+	"github.com/flamego/flamego/internal/route"
+)
+
+// Context is the runtime context of the coming request, and provide handy
+// methods to enhance developer experience.
 type Context interface {
-	// todo
+	inject.Injector
+	// URLPath builds the "path" portion of URL with given pairs of values. To
+	// include the optional segment, pass `"withOptional", "true"`.
+	//
+	// This is a transparent wrapper of Router.URLPath.
+	URLPath(name string, pairs ...string) string
+	// Written returns whether or not the ResponseWriter has been written.
+	//
+	// This is a transparent wrapper of ResponseWriter.Written.
+	Written() bool
+
+	// Next runs the next handler in the context chain.
+	Next()
+
+	// run executes all handlers in the context chain.
 	run()
+}
+
+type context struct {
+	inject.Injector
+
+	handlers []Handler // The list of handlers to be executed.
+	action   Handler   // The last action handler to be executed.
+	index    int       // The index of the current handler that is being executed.
+
+	responseWriter ResponseWriter // The http.ResponseWriter wrapper for the coming request.
+	request        *Request       // The http.Request wrapper for the coming request.
+	params         route.Params   // The values of bind parameters for the coming request.
+
+	// urlPath is used to build URL path for a route.
+	urlPath urlPather
+}
+
+type urlPather func(name string, pairs ...string) string
+
+// newContext creates and returns a new Context.
+func newContext(w http.ResponseWriter, r *http.Request, params route.Params, handlers []Handler, urlPath urlPather) Context {
+	c := &context{
+		Injector:       inject.New(),
+		handlers:       handlers,
+		responseWriter: NewResponseWriter(r.Method, w),
+		request:        &Request{Request: r},
+		params:         params,
+		urlPath:        urlPath,
+	}
+	c.Map(c)
+	c.MapTo(c.responseWriter, (*http.ResponseWriter)(nil))
+	c.Map(r)
+	return c
+}
+
+func (c *context) URLPath(name string, pairs ...string) string {
+	return c.urlPath(name, pairs...)
+}
+
+// handler returns the candidate handler to be invoked.
+func (c *context) handler() Handler {
+	if c.index < len(c.handlers) {
+		return c.handlers[c.index]
+	}
+	if c.index == len(c.handlers) {
+		return c.action
+	}
+
+	panic("invalid index for context handler")
+}
+
+func (c *context) Next() {
+	c.index++
+	c.run()
+}
+
+func (c *context) Written() bool {
+	return c.responseWriter.Written()
+}
+
+func (c *context) run() {
+	for c.index <= len(c.handlers) {
+		vals, err := c.Invoke(c.handler())
+		if err != nil {
+			panic("unable to invoke handler: " + err.Error())
+		}
+		c.index++
+
+		// If the handler returned something, write it to the response.
+		if len(vals) > 0 {
+			ev := c.Value(reflect.TypeOf(ReturnHandler(nil)))
+			handleReturn := ev.Interface().(ReturnHandler)
+			handleReturn(c, vals)
+		}
+
+		if c.Written() {
+			return
+		}
+	}
 }
--- mock.go
+++ mock.go
@@ -5,17 +5,44 @@
 package flamego
 
 import (
+	"github.com/flamego/flamego/internal/inject"
 	"github.com/flamego/flamego/internal/route"
 )
 
 type mockContext struct {
+	inject.Injector
+
 	params route.Params
 
-	run_ func()
+	setParent_ func(inject.Injector)
+	urlPath_   urlPather
+	written_   func() bool
+	next_      func()
+	run_       func()
+}
+
+func newMockContext() *mockContext {
+	return &mockContext{
+		Injector: inject.New(),
+	}
+}
+
+func (c *mockContext) SetParent(injector inject.Injector) {
+	c.setParent_(injector)
+}
+
+func (c *mockContext) URLPath(name string, pairs ...string) string {
+	return c.urlPath_(name, pairs...)
+}
+
+func (c *mockContext) Written() bool {
+	return c.written_()
+}
+
+func (c *mockContext) Next() {
+	c.next_()
 }
 
 func (c *mockContext) run() {
-	if c.run_ != nil {
-		c.run_()
-	}
+	c.run_()
 }
